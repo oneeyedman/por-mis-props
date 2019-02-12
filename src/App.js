02ed2cf75c9eda321adb64fbbe8361aaa3c23fda
Carlos Mañas <carlos@spacenomads.com>
2019-02-12 17:48:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+  import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      label: 'Botón',
+      selectedBTN: ''
+    };
+    this.getTotal = this.getTotal.bind(this);
+    this.getMyText = this.getMyText.bind(this);
+  }
+
+  getTotal(e) {
+    let newTotal = e.currentTarget.value;
+    if (newTotal === '') {
+      newTotal = 0;
+    } else {
+      newTotal = parseInt(newTotal);
+    }
+    this.setState({
+      total: newTotal
+      })
+  }
+   getMyText(e) {
+     const btnText = e.currentTarget.innerHTML;
+     this.setState({
+       selectedBTN: btnText
+     });
+   }
   render() {
+    const {total, label, selectedBTN} = this.state;
+    let buttons = [];
+    for (let i=0;i < total; i++) {
+      buttons.push(<button key={i} onClick={this.getMyText}>{label} nº{i+1}</button>);
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="app__field">
+          <input type="number" onKeyUp={this.getTotal} />
+        </div>
+        <p>Texto del botón pulsado: {selectedBTN}</p>
+        <div className="app__buttons">{buttons}</div>
       </div>
     );
   }
